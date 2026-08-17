ecbe43ebc3e7abf876195ad577bdfc28cfb53277
shun62201234-crypto <shun.622.01234@gmail.com>
2026-08-18 07:37:34 +0900

    index.ts作成
    型・interfaceを定義

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// === 型・interfaceを定義 ===
+export interface TimerHistory {
+    id: string;
+    hours: number;
+    minutes: number;
+    seconds: number;
+}
+
+export interface TimerState {
+    id: string;
+    hours: number;
+    minutes: number;
+    // タイマーが有効かどうかを示すプロパティを追加
+    enabled: boolean; 
+}
